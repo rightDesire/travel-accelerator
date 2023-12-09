0ad42290851e17f7c11dacd86cb26224c62da1ae
Egor <105357627+rightDesire@users.noreply.github.com>
2023-12-09 19:40:04 +0300

    Добавляет реализацию Tours

--- source/js/modules/init-sliders.js
+++ source/js/modules/init-sliders.js
@@ -1,11 +1,13 @@
 import {initHeroSlider} from './hero-slider';
+import {initToursSlider} from './tours-slider';
 
-const vpDesktop = 1440;
-const vpTablet = 1200;
-const vpMobile = 768;
+const vpDesktop = 1200;
+const vpTablet = 768;
+const vpMobile = 320;
 
 const loop = {
   loopHero: false, // Тестируем
+  loopTours: false,
 };
 
 const breakpointsHero = {
@@ -20,6 +22,25 @@ const breakpointsHero = {
   },
 };
 
+const breakpointsTours = {
+  [vpDesktop]: {
+    allowTouchMove: false,
+    slidesPerView: 3,
+    spaceBetween: 30,
+  },
+  [vpTablet]: {
+    allowTouchMove: true,
+    slidesPerView: 2,
+    spaceBetween: 18,
+  },
+  [vpMobile]: {
+    allowTouchMove: true,
+    slidesPerView: 1,
+    spaceBetween: 15,
+  },
+};
+
 export const initSliders = () => {
   initHeroSlider(loop.loopHero, breakpointsHero);
+  initToursSlider(loop.loopTours, breakpointsTours);
 };
